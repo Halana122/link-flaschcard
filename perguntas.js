65374a1d30d9b0938dbf580053a37a72541555f0
Halana122 <halana.correia.silva@escola.pr.gov.br>
2025-05-20 15:15:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Educação Financeira',
+    'O que é educação financeira?',
+    'É o conjunto de conhecimentos que ajuda as pessoas a administrar melhor o dinheiro, planejar gastos, poupar e investir com consciência.'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'Qual a importância de fazer um orçamento pessoal ou familiar?',
+    'O orçamento permite controlar gastos, evitar dívidas e identificar formas de economizar e alcançar objetivos financeiros.'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'O que significa viver dentro do padrão de vida?',
+    'Significa gastar menos ou igual ao que se ganha, evitando dívidas e mantendo uma vida financeira equilibrada.'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'O que é a reserva de emergência e por que ela é importante?',
+    'É quantia de dinheiro guardada para imprevistos como desemprego ou problemas de saúde, e evita endividamento em momentos de crise.'
+)
+criaCartao(
+    'Educação Financeira',
+    'Como podemos diferenciar um gasto essencial de um gasto supérfulo?',
+    'Um gasto essencial é necessário para viver bem como alimentação, moradia e transporte. Já um gasto supérfulo é algo que não é indispensável, como comprar roupas da moda ou fazer passeios caros frequentemente.'
+)
+criaCartao(
+    'Educação Financeira',
+    'Por que é importante começar a poupar desde cedo?',
+    'Porque o hábito de poupar ajudar a conquiatar metas, ter segurança financeira no futuro e aproveitar os juros compostos nos investimemntos'
+)
